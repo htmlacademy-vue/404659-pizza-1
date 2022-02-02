30c5df1a4a8f09eb7150c4de73cdd79860e394aa
Konstantin Podolskiy <kpodolsckij@yandex.ru>
2022-02-03 01:24:39 +0300

    Добавлены константы для ингредиентов. Реализовал список ингредиентов через v-for

---
+++ src/frontend/src/common/constants.js
@@ -0,0 +1,47 @@
+export const INGREDIENTS = {
+  Грибы: {
+    name: "mushrooms",
+  },
+  Чеддер: {
+    name: "cheddar",
+  },
+  Салями: {
+    name: "salami",
+  },
+  Ветчина: {
+    name: "ham",
+  },
+  Ананас: {
+    name: "ananas",
+  },
+  Бекон: {
+    name: "bacon",
+  },
+  Лук: {
+    name: "onion",
+  },
+  Чили: {
+    name: "chile",
+  },
+  Халапеньо: {
+    name: "jalapeno",
+  },
+  Маслины: {
+    name: "olives",
+  },
+  Томаты: {
+    name: "tomatoes",
+  },
+  Лосось: {
+    name: "salmon",
+  },
+  Моцарелла: {
+    name: "mozzarella",
+  },
+  Пармезан: {
+    name: "parmesan",
+  },
+  "Блю чиз": {
+    name: "blue_cheese",
+  },
+};
