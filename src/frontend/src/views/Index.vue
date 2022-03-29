f7dd8aaa70962e77c51332940f597b9d813f1d5c
konstantin-podolsckij <kpodolsckij@yandex.ru>
2022-03-29 23:39:14 +0300

    Созданы компоненты для отрисовки пиццы и расчета цены

--- src/frontend/src/views/Index.vue
+++ src/frontend/src/views/Index.vue
@@ -12,32 +12,7 @@
             :sauces="sauces"
             :ingredients="ingredients"
           />
-
-          <div class="content__pizza">
-            <label class="input">
-              <span class="visually-hidden">Название пиццы</span>
-              <input
-                type="text"
-                name="pizza_name"
-                placeholder="Введите название пиццы"
-              />
-            </label>
-
-            <div class="content__constructor">
-              <div class="pizza pizza--foundation--big-tomato">
-                <div class="pizza__wrapper">
-                  <div class="pizza__filling pizza__filling--ananas"></div>
-                  <div class="pizza__filling pizza__filling--bacon"></div>
-                  <div class="pizza__filling pizza__filling--cheddar"></div>
-                </div>
-              </div>
-            </div>
-
-            <div class="content__result">
-              <p>Итого: 0 ₽</p>
-              <button type="button" class="button" disabled>Готовьте!</button>
-            </div>
-          </div>
+          <BuilderPizzaContent />
         </div>
       </form>
     </main>
@@ -56,6 +31,7 @@ import AppLayout from "@/layouts/AppLayout.vue";
 import BuilderDoughSelector from "@/modules/builder/components/BuilderDoughSelector";
 import BuilderSizeSelector from "@/modules/builder/components/BuilderSizeSelector";
 import BuilderIngredientsSelector from "@/modules/builder/components/BuilderIngredientsSelector";
+import BuilderPizzaContent from "@/modules/builder/components/BuilderPizzaContent";
 
 export default {
   data() {
@@ -71,6 +47,7 @@ export default {
     BuilderDoughSelector,
     BuilderSizeSelector,
     BuilderIngredientsSelector,
+    BuilderPizzaContent,
   },
 };
 </script>
