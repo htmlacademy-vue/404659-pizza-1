b383614e7e21eb9f8170a25d75cc61c5d641e4a6
konstantin-podolsckij <kpodolsckij@yandex.ru>
2022-04-13 00:21:36 +0300

    Добавлены заглушки и маршруты для страниц Авторизации, Истории заказов и Корзины

--- src/frontend/src/router/routes.js
+++ src/frontend/src/router/routes.js
@@ -7,4 +7,28 @@ export default [
       layout: "AppLayoutMain",
     },
   },
+  {
+    path: "/login",
+    name: "Login",
+    component: () => import("@/views/Login.vue"),
+    meta: {
+      layout: "AppLayoutDefault",
+    },
+  },
+  {
+    path: "/orders",
+    name: "Orders",
+    component: () => import("@/views/Orders.vue"),
+    meta: {
+      layout: "AppLayoutMain",
+    },
+  },
+  {
+    path: "/cart",
+    name: "Cart",
+    component: () => import("@/views/Cart.vue"),
+    meta: {
+      layout: "AppLayoutMain",
+    },
+  },
 ];
